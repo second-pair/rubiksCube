fd6bf2da7d320d16af53fbfa1d786864723f74f9
Blair Edwards <blaired1@yahoo.co.uk>
2018-12-17 04:41:55 +0000

    Turns out my cube was fine, I'd just culled out the faces that were hidden on the other side of the cube. Played around with the numbers, getting a feel for good sizes & parameters. Added some nice colours.

--- rubiksCube.py
+++ rubiksCube.py
@@ -31,10 +31,10 @@ window = pyglet .window .Window ()
 
 # One-time GL setup
 def setup ():
-	glClearColor (1, 1, 1, 1)
-	glColor3f (1, 0, 0)
+	glClearColor (40.0 / 255, 43.0 / 255, 53.0 / 255, 0)
+	glColor3f (25.0 /255, 196.0 / 255, 241.0 / 255)
 	glEnable (GL_DEPTH_TEST)
-	glEnable (GL_CULL_FACE)
+#	glEnable (GL_CULL_FACE)
 
 
 #  Really this is an Init thing, despite being on_resize
@@ -45,16 +45,16 @@ def on_resize (width, height):
 	glViewport (0, 0, width, height)
 	glMatrixMode (GL_PROJECTION)
 	glLoadIdentity ()
-	gluPerspective (60., width / float (height), .1, 1000.)
+	gluPerspective (90., width / float (height), .1, 1000.)
 	glMatrixMode (GL_MODELVIEW)
 	return pyglet .event .EVENT_HANDLED
 
 #  Update our viewport's rotation
 def update (dt):
 	global rx, ry, rz
-	rx += dt * 1
-	ry += dt * 80
-	rz += dt * 30
+	rx += dt * 50
+	ry += dt * 40
+	rz += dt * 15
 	rx %= 360
 	ry %= 360
 	rz %= 360
@@ -67,13 +67,13 @@ def on_draw ():
 #	window .clear ()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 	glLoadIdentity()
-	glTranslatef(0, 0, -4)
+	glTranslatef(0, 0, -100)
 #	glRotatef (30, 1, 0, 0)
 #	glRotatef (30, 0, 1, 0)
 #	glRotatef (30, 0, 0, 1)
-	glRotatef(rz, 0, 0, 1)
-	glRotatef(ry, 0, 1, 0)
-	glRotatef(rx, 1, 0, 0)
+	glRotatef (rz, 0, 0, 1)
+	glRotatef (ry, 0, 1, 0)
+	glRotatef (rx, 1, 0, 0)
 	cubesBatch .draw ()
 
 setup ()
@@ -113,6 +113,18 @@ cube1 = cubesBatch .add (24, pyglet .gl .GL_QUADS, None, ("v3i", (
 	50, 80, 80,
 	80, 80, 80,
 	80, 50, 80
+	)))
+
+cube2 = cubesBatch .add (8, pyglet .gl .GL_QUADS, None, ("v3i", (
+	0, 0, 100,
+	0, 10, 100,
+	10, 10, 100,
+	10, 0, 100,
+	
+	0, 0, 0,
+	0, 10, 0,
+	10, 10, 0,
+	10, 0, 0
 	)))
 
 
