48282bb32b6c0f77828aa67b7499c7a603ec6e25
Blair Edwards <blaired1@yahoo.co.uk>
2018-12-17 01:57:59 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+#  Rubik's Cube
+This is intended to be a Rubik's Cube solver, but may end up being other things.
+This is mainly a challenge project.
---
+++ rubiksCube.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+#  Author:  Blair Edwards 2018
+#  For now, this is a programme that will end up solving Rubik's Cubes of arbitrary size.
+#  I intend that the programme will display a cube being solved and output a list of moves.
+#  I'll also need to implement a shuffling algorithm.
+
+import pyglet
+
+print ("Hello, world!")
