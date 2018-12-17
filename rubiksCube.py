9f2d9807f18fda9270e52814b61e282f7ed24942
Blair Edwards <blaired1@yahoo.co.uk>
2018-12-17 03:03:00 +0000

    Managed to render some square-looking primitives. Decided to jump straight in with graphics .batch () rendering, since it's apparently the most efficient.

--- rubiksCube.py
+++ rubiksCube.py
@@ -19,18 +19,30 @@ window = pyglet .window .Window ()
 #  Pyglet Render
 @window .event
 def on_draw ():
+	pass
 	window .clear ()
-	label .draw ()
+#	label .draw ()
+#	graphics .Batch ()
+	cubesBatch .draw ()
 
 #  For now, we'll just start by rendering some text.
-label = pyglet .text .Label (
-	"Hello, world!",
-	font_name = "Calibri",
-	font_size = 36,
-	x = window .width // 2,
-	y = window .height // 2,
-	anchor_x = "center",
-	anchor_y = "center"
-	)
+#label = pyglet .text .Label (
+#	"Hello, world!",
+#	font_name = "Calibri",
+#	font_size = 36,
+#	x = window .width // 2,
+#	y = window .height // 2,
+#	anchor_x = "center",
+#	anchor_y = "center"
+#	)
 
+#  Let's add some cubes (this should become a class later).
+#  Well, they're just squares for now.
+cubesBatch = pyglet .graphics .Batch ()
+
+cube1 = cubesBatch .add (4, pyglet .gl .GL_QUADS, None, ("v2i", (10, 10, 10, 40, 40, 40, 40, 10)))
+cube2 = cubesBatch .add (4, pyglet .gl .GL_QUADS, None, ("v2i", (80, 80, 80, 120, 120, 120, 120, 80)))
+
+
+#  Setup complete!
 pyglet .app .run ()
